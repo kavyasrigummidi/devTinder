09525c9d1040589f869fc5fcbce64d0bce88b745
kavyasrigummidi <kavyagummidi@gmail.com>
2024-12-28 11:57:58 +0530

    exploring  get,post,delete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+app.get("/user", (req,res) => {
+    res.send("hello Hello!");
+});
 
+app.post("/user", (req,res) => {
+    res.send("hello Hello Kavya!");
+});
+
+app.delete("/user", (req,res) => {
+    res.send("deleted successfully!");
+});
 app.use("/test", (req, res) => {
     res.send("server is connected to wedpage");
 });
